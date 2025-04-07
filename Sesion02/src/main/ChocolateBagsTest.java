27c0707d400202fb30c28cd3eab854b9023d9f2f
AndresCT1 <carlos.calvo@ucsm.edu.pe>
2025-04-06 22:07:40 -0500

    Chore: Ejercicio 2 resuelto: Las g felices

--- Sesion02/src/main/ChocolateBagsTest.java
+++ Sesion02/src/main/ChocolateBagsTest.java
@@ -23,14 +23,6 @@ public class ChocolateBagsTest {
     }
 
     @ParameterizedTest
-    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
-    public void totalIsBigger(int small, int big, int total, int expected) {
-        ChocolateBags bags = new ChocolateBags();
-        int result = bags.calculate(small, big, total);
-        assertEquals(expected, result);
-    }
-
-    @ParameterizedTest
     @CsvSource({ "4,2,3,3", "3,2,3,3", "2,2,3,-1", "1,2,3,-1" })
     public void onlySmallBars(int small, int big, int total, int expected) {
         ChocolateBags bags = new ChocolateBags();
@@ -38,6 +30,13 @@ public class ChocolateBagsTest {
         assertEquals(expected, result);
     }
 
+    @ParameterizedTest
+    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
+    public void totalIsBigger(int small, int big, int total, int expected) {
+        ChocolateBags bags = new ChocolateBags();
+        int result = bags.calculate(small, big, total);
+        assertEquals(expected, result);
+    }
     @ParameterizedTest
     @CsvSource({ "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2" })
     public void bigAndSmallBars(int small, int big, int total, int expected) {
