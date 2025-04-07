824b01717447d96f6226a5aa3a60a61c774f8cfb
AndresCT1 <carlos.calvo@ucsm.edu.pe>
2025-04-06 21:48:02 -0500

    Chore: Agregando métodos onlySmallBars y onlyBigBars

--- Sesion02/src/main/ChocolateBags.java
+++ Sesion02/src/main/ChocolateBags.java
@@ -1,10 +1,15 @@
 package main;
 
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ChocolateBags {
     public int calculate(int small, int big, int total) {
         int maxBigBoxes = total / 5;
-        int bigBoxesWeCanUse = maxBigBoxes < big ? maxBigBoxes : big;
-        total -= (bigBoxesWeCanUse * 5);
+        int bigBoxes = maxBigBoxes < big ? maxBigBoxes : big;
+        total -= (bigBoxes * 5);
         if(small < total)
             return -1;
         return total;
